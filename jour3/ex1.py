1f24144981a8864c700ce64bffea6c8bacbb8ca0
SayhanIbrahim <ibrahim.sayhan@laplateforme.io>
2020-10-22 00:45:33 +0200

    dernier

--- jour3/ex1.py
+++
@@ -1,8 +0,0 @@
-example_dict = {1: 'a', 2: 'b', 3: 'c', 4: 'd'}
-list1 = []
-for i, k in enumerate(example_dict):
-    print(i, k)
-for i, (k, v) in enumerate(example_dict.items()):
-    mult = k * v
-    list1.append(mult)
-print(list1)
